3e0b450d083c0bdacd6ad71cd0886309faa3524a
Adnan <adnan.chowdhury@outlook.com>
2019-12-03 21:05:47 -0600

    even or odd function

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,17 @@
 
 # YOUR CODE HERE
 
+
+def checkEven(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
+checkEven(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
-
 # YOUR CODE HERE
-
